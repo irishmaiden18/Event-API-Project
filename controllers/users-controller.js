987a263efff1f7cd5178485513c622c975883189
irishmaiden18 <laurapohl18@gmail.com>
2026-03-29 18:33:50 -0400

    adds user CRUD functionality

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,6 +1,53 @@
 // import the Users model
 const User = require("../models/users-model");
 
+// write a function that will return all users
+// everything that has to do with our database requires async/await
+const getAllUsers = async () => {
+
+    try {
+        
+        // get a list of all the users
+        const users = await User.find()
+
+        // return all the users
+        return users
+
+    } catch (error) {
+        
+        // propogates the error to the router file
+        throw error
+
+    }
+}
+
+// write a function that will take in a userID and return the data associated with that user
+// everything that has to do with our database requires async/await
+const getUserById = async (userId) => {
+
+    try {
+        
+        // find a user by the given userId
+        const user = await User.findById(userId)
+
+        // if there is no user found
+        if (!user) {
+
+            // throw an error
+            throw Error ("User NOT found")
+        }
+
+        // return the found user
+        return user
+
+    } catch (error) {
+     
+        // propogates the error to the router file
+        throw error
+
+    }
+}
+
 // write a function that will take in userData and create a new data entry based on that data
 // everything that has to do with our database requires async/await
 const createUser = async (userData) => {
@@ -20,5 +67,63 @@ const createUser = async (userData) => {
     }
 }
 
+// write a function that will edit an existin user data entry based on given data
+// everything that has to do with our database requires async/await
+const updateUser = async (userId, userData) => {
+
+    try {
+
+        // find the user by id and update using given userData
+        // new: true makes the function return the updated entry rather than the initial entry
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            userData,
+            {new: true}
+        )
+
+        // if no updated user is found
+        if (!updatedUser) {
+
+            // throw an error
+            throw Error ("User NOT found!")
+        }
+
+        // return the updated user
+        return updatedUser
+        
+    } catch (error) {
+        
+        // propogates the error to the router file
+        throw error
+    }
+}
+
+// write a function to delete a user given an id
+// anything that has to do with our database requires async/await
+const deleteUser = async (userId) => {
+
+    try {
+
+        // find the user by id and delete it
+        const userToDelete = await User.findByIdAndDelete(userId)
+
+        // if no userToDelete found
+        if (!userToDelete) {
+
+            // throw an error
+            throw Error ("User NOT found!")
+        }
+        
+        // return the userToDelete
+        return userToDelete
+
+    } catch (error) {
+        
+        // propogates the error to the router file
+        throw error
+
+    }
+}
+
 // export the controller functions
-module.exports = { createUser };
+module.exports = { createUser, getAllUsers, getUserById, updateUser, deleteUser };
--- routes/users-router.js
+++ routes/users-router.js
@@ -5,11 +5,63 @@ const express = require("express");
 const router = express.Router();
 
 // import controller functionality
-const { createUser } = require("../controllers/users-controller");
+const { createUser, getAllUsers, getUserById, updateUser, deleteUser } = require("../controllers/users-controller");
+
+// handle GET requests to /api/v1/users
+// anything that has to do with our database needs async/await
+router.get("/", async (req, res) => {
+    
+    try {
+    
+        // call the getAllUsers users-controller function
+        const allUsers = await getAllUsers()
+
+        // send a success response to the user
+        res.json ({
+            message: "success",
+            payload: allUsers
+        })
+
+    } catch (error) {
+
+        // send a failure message to the user
+        res.status(500).json ({
+            message: "failure",
+            payload: error.message
+        })
+        
+    }
+})
+
+// handle GET routes with an ID /api/v1/users/:id
+// anything that has to do with our database needs async/await
+router.get("/:id", async (req, res) => {
+
+    try {
+        
+        // call the getUserById users-controller function
+        // anything that has to do with our database needs async/await
+        const user = await getUserById(req.params.id)
+
+        // send a success response to the user
+        res.json ({
+            message: "success",
+            payload: user
+        })
+
+    } catch (error) {
+        
+        // send a failure response to the user
+        res.status(404).json ({
+            message: "failure",
+            paylod: error.message
+        })
+    }
+})
 
 // handle POST requests to /api/v1/users
 // anything that has to do with our database needs async/await
-router.post("/", async (req,res) => {
+router.post("/", async (req, res) => {
 
     try {
 
@@ -26,6 +78,56 @@ router.post("/", async (req,res) => {
         
         // send a failure response to the user
         res.status(500).json ({
+            message: "failure",
+            payload: error.message
+        })
+    }
+})
+
+// handle PUT requests to /api/v1/users/:id
+// anything that has to do with our database needs async/await
+router.put("/:id", async (req, res) => {
+
+    try {
+        
+        // call the updateUser users-controller function
+        const updatedUser = await updateUser(req.params.id, req.body)
+
+        // send a success response to the user
+        res.json ({
+            message: "success",
+            payload: updatedUser
+        })
+
+    } catch (error) {
+        
+        // send a failure response to the user
+        res.status(404).json ({
+            message: "failure",
+            payload: error.message
+        })
+    }
+})
+
+// handle DELETE requests to /api/v1/users/:id
+// anything that has to do with our database needs async/await
+router.delete("/:id", async (req, res) => {
+
+    try {
+        
+        // call the users controller function deleteUser
+        const userToDelete = await deleteUser(req.params.id)
+
+        // send a success message to the user
+        res.json ({
+            message: "success",
+            payload: `${userToDelete.username} has been successfully removed from the database!`
+        })
+        
+    } catch (error) {
+        
+        // send a failure response to the user
+        res.status(404).json ({
             message: "failure",
             payload: error.message
         })
